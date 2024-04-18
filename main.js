7424e03632ff0b1a56c5662d29aef1f472d4a930
Seoyeong Min <mseo1123@naver.com>
2024-04-18 23:25:23 +0200

    Feat: Added life and score, changed the cactus style

--- main.js
+++ main.js
@@ -7,9 +7,6 @@ canvas.height = window.innerHeight - 100;
 let img1 = new Image();
 img1.src = 'dino.png';
 
-let img2 = new Image();
-img2.src = 'cactus.png';
-
 let dino = {
   x: 100,
   y: 200,
@@ -22,13 +19,14 @@ let dino = {
 
 class Cactus {
   constructor() {
-    this.width = 50;
-    this.height = 50;
+    this.width = 20 + getRandomInt(-3, 4);
+    this.height = 30 + getRandomInt(-3, 4);
     this.x = 500;
     this.y = 250 - this.height;
   }
   draw() {
-    ctx.drawImage(img2, this.x, this.y);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(this.x, this.y, this.width, this.height);
   }
 }
 
@@ -38,9 +36,12 @@ cactus.draw();
 // Animation(obstacle generation, dino jump)
 let timer = 0;
 let cactusArr = [];
+let gameState = 0; // 0: end, 1: start
 let jump = false;
 let jumpTimer = 0;
 let gameFrame;
+let life = 5;
+let score = 0;
 
 function animation() {
   gameFrame = requestAnimationFrame(animation);
@@ -49,7 +50,7 @@ function animation() {
 
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  if (timer % 100 === 0) {
+  if (timer % 200 === 0) {
     let cactus = new Cactus();
     cactusArr.push(cactus);
   }
@@ -57,11 +58,12 @@ function animation() {
   cactusArr.forEach((a, i, o) => {
     if (a.x < 0) {
       o.splice(i, 1);
+      score += 10;
+      document.querySelector('#score').innerHTML = score;
+    } else if (collisionDetection(dino, a) < 0) {
+      o.splice(i, 1);
     }
-    a.x--;
-
-    collisionDetection(dino, a);
-
+    a.x -= 2;
     a.draw();
   });
 
@@ -90,13 +92,34 @@ function collisionDetection(dino, cactus) {
   let x = cactus.x - (dino.x + dino.width);
   let y = cactus.y - (dino.y + dino.height);
   if (x < 0 && y < 0) {
-    cancelAnimationFrame(gameFrame);
-    alert('Game over');
+    life--;
+    document.querySelector('#life').innerHTML = life;
+    if (life == 0) {
+      alert(`Game over. You got ${score} points`);
+      gameState = 0;
+      cancelAnimationFrame(animation);
+      location.reload();
+    }
+    return -1;
+  } else {
+    return 1;
   }
 }
 
-document.addEventListener('keydown', function (e) {
-  if (e.code === 'Space') {
-    jump = true;
+document.addEventListener('keydown', (e) => {
+  if (e.code == 'Space') {
+    if (gameState == 0) {
+      gameState = 1;
+      frameAction();
+      document.querySelector('#h2').style.display = 'none';
+    } else if (gameState == 1) {
+      jump = true;
+    }
   }
 });
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
